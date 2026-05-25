47a72bd41cc6c62e3721f9813a394f093383e121
Shamshuu <shamshu2004@gmail.com>
2026-05-25 18:10:12 +0530

    Update test-demo.sh

--- test-demo.sh
+++ test-demo.sh
@@ -1,7 +1,7 @@
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 RED='\033[0;31m'
-NC='\033[0m' # No Color
+NC='\033[0m' 
 
 echo -e "${BLUE}==================================================${NC}"
 echo -e "${BLUE}   MULTI-TENANT ROUTING PROJECT DEMO             ${NC}"
